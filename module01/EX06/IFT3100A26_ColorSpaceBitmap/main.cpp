42c7aafa9715b055a163198d2e50387fdbd97269
philvoyer <philvoyer@subsequent.dev>
2026-09-09 11:56:22 -0400

    publication exemple 1.6 (ColorSpaceBitmap)

--- module01/EX06/IFT3100A26_ColorSpaceBitmap/main.cpp
+++ module01/EX06/IFT3100A26_ColorSpaceBitmap/main.cpp
@@ -1,4 +1,4 @@
-// IFT3100A25_ColorSpaceBitmap\main.cpp
+// IFT3100A26_ColorSpaceBitmap/main.cpp
 // Génération procédurale d'une image dans un espace de couleur binaire.
 // Les pixels sont colorés selon le pattern d'un échiquier.
 // Exportation des données de l'image dans un fichier .pbm (portable bitmap).
