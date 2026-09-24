a368e44f8ee6e1c0bfaf9678ffad7b4163c4aef4
philvoyer <philvoyer@subsequent.dev>
2026-09-24 08:50:59 -0400

    publication exemple 3.7 (GLSL_VectorDeclaration)

--- module03/EX07/IFT3100A26_GLSL_VectorDeclaration.glsl
+++ module03/EX07/IFT3100A26_GLSL_VectorDeclaration.glsl
@@ -1,4 +1,4 @@
-// IFT3100A25_GLSL_VectorDeclaration.glsl
+// IFT3100A26_GLSL_VectorDeclaration.glsl
 // Exemples de déclaration de variables de type vecteur en GLSL.
 
 // 1. déclaration d'un vecteur pour l'origine d'un espace
