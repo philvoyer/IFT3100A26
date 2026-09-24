a60ffb737324a780aa183f79dab0fa9f458a26f0
philvoyer <philvoyer@subsequent.dev>
2026-09-24 08:54:31 -0400

    publication exemple 3.8 (GLSL_VectorComponent)

--- module03/EX08/IFT3100A26_GLSL_VectorComponent.glsl
+++ module03/EX08/IFT3100A26_GLSL_VectorComponent.glsl
@@ -1,4 +1,4 @@
-// IFT3100A25_GLSL_VectorComponent.glsl
+// IFT3100A26_GLSL_VectorComponent.glsl
 // Exemples d’accès aux composantes d’un vecteur en GLSL.
 
 // déclaration de variables
@@ -16,53 +16,53 @@ v[0] = 1.0;
 v[1] = 2.0;
 v[2] = 3.0;
 
-// 3. lecture par étiquette de composante de transformation
+// 3. lecture par étiquette de composante de transformation (XYZ)
 component = v.x;
 component = v.y;
 component = v.z;
 
-// 4. écriture par étiquette de composante de transformation
+// 4. écriture par étiquette de composante de transformation (XYZ)
 v.x = 1.0;
 v.y = 2.0;
 v.z = 3.0;
 
-// 5. lecture par étiquette de composante de couleur
+// 5. lecture par étiquette de composante de couleur (RGB)
 component = v.r;
 component = v.g;
 component = v.b;
 
-// 6. écriture par étiquette de composante de couleur
+// 6. écriture par étiquette de composante de couleur (RGB)
 v.r = 1.0;
 v.g = 2.0;
 v.b = 3.0;
 
-// 7. lecture par étiquette de composante de texture (uvw ou stp)
-component = v.u;
-component = v.v;
-component = v.w;
+// 7. lecture par étiquette de composante de texture (STPQ)
+component = v.s;
+component = v.t;
+component = v.p;
 
-// 8. écriture par étiquette de composante de texture
-v.u = 1.0;
-v.v = 2.0;
-v.w = 3.0;
+// 8. écriture par étiquette de composante de texture (STPQ)
+v.s = 1.0;
+v.t = 2.0;
+v.p = 3.0;
 
-// 9. swizzle par étiquette de composante de transformation
+// 9. swizzle par étiquette de composante de transformation (XYZ)
 result = v.xyz; // result = (1.0, 2.0, 3.0)
 result = v.zyx; // result = (3.0, 2.0, 1.0)
 result = v.xzy; // result = (1.0, 3.0, 2.0)
 result = v.yxz; // result = (2.0, 1.0, 3.0)
 result = v.xxx; // result = (1.0, 1.0, 1.0)
 
-// 10. swizzle par étiquette de composante de couleur
+// 10. swizzle par étiquette de composante de couleur (RGB)
 result = v.rgb; // result = (1.0, 2.0, 3.0)
 result = v.bgr; // result = (3.0, 2.0, 1.0)
 result = v.rbg; // result = (1.0, 3.0, 2.0)
 result = v.grb; // result = (2.0, 1.0, 3.0)
 result = v.rrr; // result = (1.0, 1.0, 1.0)
 
-// 11. swizzle par étiquette de composante de texture
-result = v.uvw;  // result = (1.0, 2.0, 3.0)
-result = v.wvu;  // result = (3.0, 2.0, 1.0)
-result = v.uwv;  // result = (1.0, 3.0, 2.0)
-result = v.vuw;  // result = (2.0, 1.0, 3.0)
-result = v.uuu;  // result = (1.0, 1.0, 1.0)
+// 11. swizzle par étiquette de composante de texture (STPQ)
+result = v.stp;  // result = (1.0, 2.0, 3.0)
+result = v.pts;  // result = (3.0, 2.0, 1.0)
+result = v.spt;  // result = (1.0, 3.0, 2.0)
+result = v.tsp;  // result = (2.0, 1.0, 3.0)
+result = v.sss;  // result = (1.0, 1.0, 1.0)
